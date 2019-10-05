483fe9e6f7059c49aac73d41e1f07bd004a1ec07
Victoria Rozhkova <vvrozhkova@gmail.com>
2019-10-05 23:08:52 +0300

    updates

--- src/components/ArticleCategory.vue
+++ src/components/ArticleCategory.vue
@@ -1,36 +1,37 @@
 <template>
-    <g-link :to="article.category.path" class="article-category__link" itemprop="articleSection">
-        <strong>
-            [ {{article.category.title.toUpperCase()}} ]
-        </strong>
-        <time itemprop="datePublished" :datetime="article.date" class="text-muted">{{new Date(article.date).toLocaleDateString(undefined, {
-            year: "numeric",
-            month: "long",
-            day: "numeric"
-            })}}
-        </time>
-    </g-link>
-
+  <g-link :to="article.category.path" class="article-category__link" itemprop="articleSection">
+    <strong>[ {{article.category.title.toUpperCase()}} ]</strong>
+    <time itemprop="datePublished" :datetime="article.date" class="white">
+      {{new Date(article.date).toLocaleDateString(undefined, {
+      year: "numeric",
+      month: "long",
+      day: "numeric"
+      })}}
+    </time>
+  </g-link>
 </template>
 
 <script>
-    export default {
-        name: "ArticleCategory",
-        props: [
-            "article"
-        ]
-    }
+export default {
+  name: "ArticleCategory",
+  props: ["article"]
+};
 </script>
 
 <style scoped>
-
-    .article-category__link {
-        color: #0052cc;
-        font-size: inherit;
-    }
-
-    time {
-        font-weight: lighter;
-    }
-
+.article-category__link {
+  color: #0052cc;
+  font-size: inherit;
+  display: table;
+}
+strong {
+  display: table-cell;
+  vertical-align: middle;
+}
+time {
+  font-size: 80%;
+  display: table-cell;
+  vertical-align: middle;
+  padding: 0 0.3vw;
+}
 </style>
