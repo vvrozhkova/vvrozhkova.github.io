e1bb8a7d151f82e5f907a972b95a403e32e1a6aa
Victoria Rozhkova <vvrozhkova@gmail.com>
2019-10-15 20:04:32 +0300

    add vue components and scroll by table contents

---
+++ src/components/post_items/PostImage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="terminal" class="screen">
+    <div class="window">
+      <slot />
+      <span class="comment">{{comment}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["comment"]
+};
+</script>
