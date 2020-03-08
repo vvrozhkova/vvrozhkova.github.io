34af229e638c53298bbed6cad9d05320224eefa0
Victoria Rozhkova <vvrozhkova@gmail.com>
2020-03-08 22:30:14 +0300

    add header and logo

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="layout">
-    <header class="header">
-      <strong>
-        <g-link to="/">{{ $static.metadata.siteName }}</g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link" to="/">Home</g-link>
-        <g-link class="nav__link" to="/about/">About</g-link>
-      </nav>
-    </header>
-    <slot/>
+  <div id="app">
+    <Header />
+    <slot />
   </div>
 </template>
+
+<script>
+import Header from "./partials/Header.vue";
+
+export default {
+  components: {
+    Header
+  }
+};
+</script>
 
 <static-query>
 query {
@@ -21,30 +23,14 @@ query {
 }
 </static-query>
 
-<style>
-body {
-  font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  margin:0;
-  padding:0;
-  line-height: 1.5;
-}
-
-.layout {
-  max-width: 760px;
-  margin: 0 auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.header {
+<style scoped>
+#app {
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-  height: 80px;
+  flex-direction: column;
+  min-height: 100vh;
 }
 
-.nav__link {
-  margin-left: 20px;
+#app main {
+  flex: 1;
 }
 </style>
---
+++ src/layouts/partials/Header.vue
@@ -0,0 +1,43 @@
+<template>
+  <header>
+    <div class="header-inner container flex gap-30">
+      <Logo />
+      <SearchForm />
+    </div>
+  </header>
+</template>
+
+
+<script>
+import Logo from "./Logo.vue";
+
+export default {
+  components: {
+    Logo
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+header {
+  z-index: 20;
+  position: relative;
+  top: 0;
+  background-color: var(--bg-transparent);
+  border-bottom: 1px solid var(--border-color);
+  flex-wrap: nowrap;
+  position: sticky;
+  transition: background-color 0.3s, border-color, 0.3s;
+  .header-inner {
+    padding: 0 var(--space);
+    min-height: var(--header-height);
+  }
+
+  @media screen and (min-width: 992px) and (max-resolution: 1) {
+    backdrop-filter: blur(4px);
+  }
+  @media screen and (min-width: 992px) and (-webkit-max-device-pixel-ratio: 1) {
+    backdrop-filter: blur(4px);
+  }
+}
+</style>
