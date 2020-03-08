e0ef1d0ec07ca0298c4da5254ebc7df0794b85da
Victoria Rozhkova <vvrozhkova@gmail.com>
2020-03-08 22:55:26 +0300

    search backend support

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,10 +1,85 @@
 // This is where project configuration and plugin options are located. 
 // Learn more: https://gridsome.org/docs/config
 
-// Changes here require a server restart.
+// Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome',
-  plugins: []
+  siteName: 'TestAutomation.Space',
+  siteUrl: 'https://www.testautomation.space',
+  siteDescription: 'Блог про автоматизацию тестирования и не только.',
+  titleTemplate: '%s - TestAutomation.Space',
+
+  plugins: [
+    {
+      use: '@gridsome/vue-remark',
+      options: {
+        typeName: 'Post',
+        baseDir: './blog/posts/ru',
+        pathPrefix: '/',
+        template: './src/templates/Post.vue',
+        route: '/:title',
+        plugins: [
+          '@gridsome/remark-prismjs'
+        ],
+        remark: {
+          autolinkHeadings: {
+            content: {
+              type: 'text',
+              value: '#'
+            }
+          }
+        },
+        refs: {
+          tags: {
+            typeName: 'Tag',
+            route: '/tag/:id',
+            create: true
+          },
+          category: {
+            typeName: 'Category',
+            route: '/category/:id',
+            create: true
+          }
+        }
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'RelatedTag',
+        path: 'blog/tags/**/*.md',
+        route: '/tag/:title',
+        refs: {
+          related: {
+            typeName: 'RelatedTagRef',
+            route: '/tag/:id',
+            create: true
+          },
+        }
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'RelatedCategory',
+        path: 'blog/categories/**/*.md',
+        route: '/category/:title',
+        refs: {
+          related: {
+            typeName: 'RelatedCategoryRef',
+            route: '/category/:id',
+            create: true
+          }
+        }
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Comment',
+        path: 'blog/comments/**/*.md'
+      }
+    }
+  ]
 }
---
+++ src/templates/Post.vue
@@ -0,0 +1,89 @@
+<template>
+  <PostLayout :subtitles="subtitles" :post="$page.post" :links="links">
+    <VueRemarkContent class="post mb"></VueRemarkContent>
+  </PostLayout>
+</template>
+
+<script>
+import PostLayout from "@/layouts/PostLayout.vue";
+import links from '@/data/doc-links.yaml'
+
+export default {
+  components: {
+    PostLayout
+  },
+  computed: {
+    links() {
+      return links
+    },
+    subtitles() {
+      // Remove h1, h4, h5, h6 titles
+      let subtitles = this.$page.post.subtitles.filter(function(
+        value,
+        index,
+        arr
+      ) {
+        return [2, 3].includes(value.depth);
+      });
+      return subtitles;
+    }
+  },
+  metaInfo() {
+    const { title, headings } = this.$page.post;
+    return {
+      title: title || (headings.length ? headings[0].value : undefined)
+    };
+  }
+};
+</script>
+
+<page-query>
+
+  query Post ($path: String!) {
+    post: post (path: $path) {
+        title
+        content
+        description
+        color
+        category{
+            id
+            title
+            path
+        }
+        date
+        imagewords
+        path
+        image
+        headings(depth: h2){
+            value
+            anchor
+        }
+        subtitles: headings {
+      depth
+      value
+      anchor
+    }
+        tags{
+            id
+            title
+            path
+        }
+    }
+
+    comments: allComment(sortBy: "date", filter:{post:{eq: $path}}){
+      edges{
+        node{
+          id
+          name
+          path
+          post
+          replies
+          content
+          date
+        }
+      }
+    }
+  }
+    
+</page-query>
+
